838156a4cc8dcc5fb80cfeddfcf5391d0d6d23a1
DJOUKENG NOUGNING YANNICK <ivanotechs@DJOUKENGs-MacBook-Pro.local>
2023-11-17 04:50:39 +0100

    first update

---
+++ app/Http/Livewire/Web/Hello.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Web;
+
+use Livewire\Component;
+
+class Hello extends Component
+{
+    public function render()
+    {
+        return view('livewire.web.hello');
+    }
+}
---
+++ resources/views/livewire/web/hello.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <div class="container">
+        <h1>HELLLOOOOOOOOOOOO</h1>
+    </div>
+    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    
+</div>
